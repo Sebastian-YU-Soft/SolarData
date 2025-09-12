ab6f55cc071eecb1b7ebe5d2ca767a85ae18f691
Sebastian <sebjerume@gmail.com>
2025-09-12 12:11:34 -0400

    updated ProfileController.java

--- src/main/java/com/maxxenergy/edap/controller/ProfileController.java
+++ src/main/java/com/maxxenergy/edap/controller/ProfileController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
 import java.util.Map;
 import java.util.HashMap;
 
